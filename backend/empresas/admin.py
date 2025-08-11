5ded74cc9602a6cf3809dba30a5cc000f540d537
Andly <andlybras.desenvolvedor@outlook.com>
2025-08-11 16:35:39 -0400

    Feat: Registra o model Empresa no Django Admin

--- backend/empresas/admin.py
+++ backend/empresas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Empresa  # Importa nosso modelo Empresa que criamos
 
 # Register your models here.
+
+# A linha abaixo registra o modelo Empresa no site de admin
+admin.site.register(Empresa)
