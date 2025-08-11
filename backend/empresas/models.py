1062ccec53cf66b9c42a0fd8eab10228e00b054e
Andly <andlybras.desenvolvedor@outlook.com>
2025-08-11 16:24:36 -0400

    Feat: Cria app 'empresas', define o model Empresa e aplica migrações

---
+++ backend/empresas/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Empresa(models.Model):
+    # Campos baseados no nosso formulário de registro e perfil
+    razao_social = models.CharField(max_length=255)
+    nome_fantasia = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=18, unique=True)
+    inscricao_estadual = models.CharField(max_length=20, blank=True, null=True) # Campo opcional
+    responsavel_nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    contatos = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True) # Para textos longos
+
+    # Campos gerenciados automaticamente pelo Django
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome_fantasia
